3dfe41be7430a7ccd89e228a1cb6f392e462bd1a
Aravind <70009354+ARTHURFLECK1828@users.noreply.github.com>
2022-01-10 21:20:26 +0530

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,27 @@
+var data = {
+  name : [
+    {name: 'Name', value: 'Aravind'}
+  ],
+  bio: [
+    {name: 'Name', value: 'Aravind P'},
+    {name: 'Email', value: 'catcharaind18@gmail.com'},
+    {name: 'Phone', value: '9344370715'},
+    {name: 'Country', value: 'India'},
+    {name: 'City', value: 'Chennai'}
+  ],
+  github: [
+    {name: 'GitHub', value: 'ARTHURFLECK1828'}
+  ],
+  projects: [
+    {name: 'Library Management System', value: 'https://github.com/ARTHURFLECK1828/Library-Managment'},
+    {name: 'A very Serious Page', value: 'https://github.com/ARTHURFLECK1828/VerySeriousPage'},
+    {name: 'CyberBonk', value: 'https://github.com/ARTHURFLECK1828/CyberBonk'}
+  ],
+  resume: 'Resume.pdf',
+  socials: [
+    {name: 'LinkedIn', value: 'https://www.linkedin.com/in/aravind-p-92a5061b8/'},
+    {name: 'GitHub', value: 'https://github.com/ARTHURFLECK1828'},
+    {name: 'Instagram', value: 'https://www.instagram.com/interwebenthusiast/'},
+    {name: 'Website', value: 'https://arthurfleck1828.github.io/Portfolio-page/'}
+  ]
+}
